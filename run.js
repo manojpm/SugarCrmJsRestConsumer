87f369ff7dc9bf75e831f94bcffdc18691148243
jakabadambalazs <jakabadambalazs@gmail.com>
2017-05-19 16:55:16 +0200

    rewritten module so we can have multiple instances

--- run.js
+++ run.js
@@ -2,10 +2,11 @@
  * Created by jack on 19/05/17.
  */
 //var Promise = require("bluebird");
-
-var sugar = require('./lib/main');
+var SugarCrmJsRestConsumer = require('./index');
 
 console.log("initing...");
+var sugar = new SugarCrmJsRestConsumer();
+
 sugar.init('http://gsi.crm.mekit.it', 'v4_1', 'user1', 'user1');
 
 
@@ -13,9 +14,13 @@ sugar.authenticate()
     .then(function()
     {
         console.log("Auth ok");
+
+
+
     })
     .catch(function(error)
     {
         console.error(error);
     });
+
 
