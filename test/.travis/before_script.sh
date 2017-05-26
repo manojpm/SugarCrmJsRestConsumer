a5a211fdd1b70fd842dcaef529b40b03eb1dca15
jakabadambalazs <jakabadambalazs@gmail.com>
2017-05-26 18:17:16 +0200

    travis tests - composer + correct suitecrm config file for db

--- test/.travis/before_script.sh
+++ test/.travis/before_script.sh
@@ -23,9 +23,16 @@ mkdir httpdocs
 git clone https://github.com/salesagility/SuiteCRM.git httpdocs
 cd httpdocs
 git checkout tags/v7.8.3
+cp /home/travis/build/adamjakab/SugarCrmJsRestConsumer/test/.travis/travis_config_si.php config_si.php
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
 composer selfupdate
 composer install --no-interaction
-cp tests/travis_config_si.php config_si.php
+
 ls -la
 php tests/testinstall.php
 
