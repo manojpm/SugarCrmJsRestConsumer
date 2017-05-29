1a68f37e3afe73b18899a82f5aea7b99b690d19f
jakabadambalazs <jakabadambalazs@gmail.com>
2017-05-29 09:25:39 +0200

    travis tests - apache - adding php support

--- test/.travis/before_script.sh
+++ test/.travis/before_script.sh
@@ -1,43 +1,45 @@
 #!/usr/bin/env bash
 
-# Init
-sudo apt-get update
-sudo apt-get install -y wget
+# Variables
+REPOPATH="$(pwd)"       # /home/travis/build/adamjakab/SugarCrmJsRestConsumer
+HTTPPATH="~/httpdocs"   # /home/travis/httpdocs
 
-# Install apache
-sudo apt-get install -y apache2
-sudo a2enmod actions
-sudo a2enmod rewrite
-sudo service apache2 restart
-
-# Install Php 5.6
-sudo apt-get install software-properties-common
+# Init & Install packages
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
-sudo apt-get install -y php5.6-cli php5.6-cgi php5.6-curl php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mbstring php5.6-mysql php5.6-xml libapache2-mod-php
-php -v
+sudo apt-get install -y wget \
+    apache2 libapache2-mod-php5.6 \
+    php5.6-cli php5.6-cgi php5.6-curl php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mbstring php5.6-mysql php5.6-xml
 
 # Get SuiteCRM
-# pwd = /home/travis/build/adamjakab/SugarCrmJsRestConsumer
-cd ~
-mkdir httpdocs
-git clone https://github.com/salesagility/SuiteCRM.git httpdocs
-cd httpdocs
-git checkout tags/v7.8.3
-cp /home/travis/build/adamjakab/SugarCrmJsRestConsumer/test/.travis/travis_config_si.php config_si.php
+mkdir ${HTTPPATH}
+cd ${HTTPPATH}
+cp ${REPOPATH}/test/.travis/index.php ./index.php
+#git clone https://github.com/salesagility/SuiteCRM.git httpdocs
+#cd httpdocs
+#git checkout tags/v7.8.3
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-php composer.phar install --no-interaction
+# Configure Php
+php -v
 
-# Execute the installer of SuiteCrm
-php tests/testinstall.php
 
 # Configure Apache
-cat /home/travis/build/adamjakab/SugarCrmJsRestConsumer/test/.travis/apache-virtualhost | sed -e "s,PATH,/home/travis/httpdocs,g" | sudo tee /etc/apache2/sites-available/default > /dev/null
+sudo a2enmod actions
+sudo a2enmod rewrite
+cat ${REPOPATH}/test/.travis/apache-virtualhost | sed -e "s,PATH,/home/travis/httpdocs,g" | sudo tee /etc/apache2/sites-available/default > /dev/null
 sudo service apache2 restart
+
+
+# Configure SuiteCrm and execute the installer
+#cp /home/travis/build/adamjakab/SugarCrmJsRestConsumer/test/.travis/travis_config_si.php config_si.php
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#php composer-setup.php
+#php -r "unlink('composer-setup.php');"
+#php tests/testinstall.php
+
+
 
 # Get
 wget -qO- http://localhost
---
+++ test/.travis/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by Adam Jakab.
+ * Date: 29/05/17
+ * Time: 9.20
+ */
+echo "PHP is working now!";
+
+phpinfo();
+
